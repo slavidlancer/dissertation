86c198d5642612d2d42bc9414805f36f30a16ec0
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-12 09:55:21 +0200

    add basic javadoc comments to the main sources

--- DissertationConsoleApplication/src/slavi/dissertation/applications/console/DissertationConsoleApplication.java
+++ DissertationConsoleApplication/src/slavi/dissertation/applications/console/DissertationConsoleApplication.java
@@ -1,6 +1,18 @@
 package slavi.dissertation.applications.console;
 
+/**
+ * DissertationConsoleApplication
+ *
+ * @author      Slavi Dimitrov
+ * @version     1.0.0
+ * @since       2016-01-11
+ */
 public class DissertationConsoleApplication {
+    /*
+     * Main method.
+     * @param args unused
+     * @return nothing
+     */
     public static void main(String[] args) {
         
     }
--- DissertationGUIApplication/src/slavi/dissertation/applications/gui/DissertationGUIApplication.java
+++ DissertationGUIApplication/src/slavi/dissertation/applications/gui/DissertationGUIApplication.java
@@ -3,9 +3,21 @@ package slavi.dissertation.applications.gui;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
+/**
+ * DissertationGUIApplication
+ *
+ * @author      Slavi Dimitrov
+ * @version     1.0.0
+ * @since       2016-01-11
+ */
 public class DissertationGUIApplication {
     private JFrame frame;
 
+    /*
+     * Main method.
+     * @param args unused
+     * @return nothing
+     */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -20,10 +32,18 @@ public class DissertationGUIApplication {
         });
     }
 
+    /*
+     * Constructor.
+     */
     public DissertationGUIApplication() {
         initialize();
     }
 
+    /*
+     * Initialize method.
+     * @return nothing
+     * @since 1.0.0
+     */
     private void initialize() {
         frame = new JFrame();
         frame.setBounds(100, 100, 450, 300);
