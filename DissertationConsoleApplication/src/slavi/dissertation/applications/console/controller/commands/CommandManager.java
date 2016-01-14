bd3a92e7a2209690fdd3e353981035aa622f299d
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-14 23:47:38 +0200

    add processCommand() method declaration

--- DissertationConsoleApplication/src/slavi/dissertation/applications/console/controller/commands/CommandManager.java
+++ DissertationConsoleApplication/src/slavi/dissertation/applications/console/controller/commands/CommandManager.java
@@ -1,5 +1,7 @@
 package slavi.dissertation.applications.console.controller.commands;
 
 public class CommandManager {
-    
+    public void processCommand(String commandLineInput) {
+        // TODO Auto-generated method stub
+    }
 }
