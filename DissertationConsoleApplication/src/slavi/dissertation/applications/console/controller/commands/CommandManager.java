6adcae9cbbc2724d8fd22b24f43af12a10915c5f
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-19 16:47:51 +0200

    add commands map, initialized with the exit command

--- DissertationConsoleApplication/src/slavi/dissertation/applications/console/controller/commands/CommandManager.java
+++ DissertationConsoleApplication/src/slavi/dissertation/applications/console/controller/commands/CommandManager.java
@@ -1,8 +1,23 @@
 package slavi.dissertation.applications.console.controller.commands;
 
+import java.util.HashMap;
+import java.util.Map;
 import slavi.dissertation.applications.console.interfaces.CommandProcessing;
+import slavi.dissertation.applications.console.interfaces.Commandable;
 
 public class CommandManager implements CommandProcessing {
+    private Map<String, Commandable> commands;
+    
+    public CommandManager() {
+        this.commands = new HashMap<String, Commandable>();
+        
+        initializeCommands();
+    }
+    
+    private void initializeCommands() {
+        commands.put("exit", new CommandExit());
+    }
+
     /*
      * Process command from the menu.
      * @param args unused
