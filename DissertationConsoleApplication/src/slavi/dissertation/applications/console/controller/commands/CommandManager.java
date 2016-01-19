ef191521ee7adab893ee8bd80c80789cb9e3fa46
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-19 16:06:44 +0200

    Add CommandProcessing interface to CommandManager

--- DissertationConsoleApplication/src/slavi/dissertation/applications/console/controller/commands/CommandManager.java
+++ DissertationConsoleApplication/src/slavi/dissertation/applications/console/controller/commands/CommandManager.java
@@ -1,6 +1,13 @@
 package slavi.dissertation.applications.console.controller.commands;
 
-public class CommandManager {
+import slavi.dissertation.applications.console.interfaces.CommandProcessing;
+
+public class CommandManager implements CommandProcessing {
+    /*
+     * Process command from the menu.
+     * @param args unused
+     * @return nothing
+     */
     public void processCommand(String commandLineInput) {
         // TODO Auto-generated method stub
     }
---
+++ DissertationConsoleApplication/src/slavi/dissertation/applications/console/interfaces/CommandProcessing.java
@@ -0,0 +1,5 @@
+package slavi.dissertation.applications.console.interfaces;
+
+public interface CommandProcessing {
+    void processCommand(String commandLineInput);
+}
